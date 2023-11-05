19bd406e0774e9f83766782523ee43788ab3696e
Gdeepika56 <dg1201@usnh.edu>
2023-11-05 18:00:05 -0500

    Add Views and templates for filter feature

--- home/views.py
+++ home/views.py
@@ -7,6 +7,8 @@ TMDB_API_KEY = "1f00b3f0d90a5ef0e263f5d2a04c4ac9"
 def search(request):
 
     query = request.GET.get('q')
+    print(query)
+
     results = []
     if query:
         data = requests.get(f"https://api.themoviedb.org/3/search/tv?api_key={TMDB_API_KEY}&language=en-US&page=1&include_adult=false&query={query}")
@@ -15,7 +17,8 @@ def search(request):
         return HttpResponse("Please enter a search query")
     
     return render(request, 'home/results.html', {
-        "data": data.json()
+        "data": data.json(),
+        "type": request.GET.get("type")
     })
 
 
