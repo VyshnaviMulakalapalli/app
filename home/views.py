03705060c5428b6d206a076206d2353f2fe46818
VyshnaviMulakalapalli <vm1096@usnh.edu>
2023-11-05 16:35:18 -0500

    Add methods in views for search feature and update urls

--- home/views.py
+++ home/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import requests
+from django.http import HttpResponse
 
-# Create your views here.
+TMDB_API_KEY = "1f00b3f0d90a5ef0e263f5d2a04c4ac9"
+
+def search(request):
+
+    query = request.GET.get('q')
+    results = []
+    if query:
+        data = requests.get(f"https://api.themoviedb.org/3/search/tv?api_key={TMDB_API_KEY}&language=en-US&page=1&include_adult=false&query={query}")
+
+    else:
+        return HttpResponse("Please enter a search query")
+    
+    return render(request, 'home/results.html', {
+        "data": data.json()
+    })
+
+
+def index(request):
+    return render(request, 'home/index.html')
