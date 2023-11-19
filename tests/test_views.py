501b7f2404b6bc163ae184214825d45f6186e8ad
VyshnaviMulakalapalli <vm1096@usnh.edu>
2023-11-19 16:17:23 -0500

    add testcases for trendings

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,7 @@
 import pytest
+import json
 from django.test import RequestFactory
-from home.views import search
+from home.views import search, view_trendings_results
 
 @pytest.mark.django_db
 def test_search_view_with_query1():
@@ -25,3 +26,23 @@ def test_search_view_without_query():
     response = search(request)
     assert response.status_code == 200
     assert 'Please enter a search query' in response.content.decode()
+
+@pytest.mark.django_db
+def test_view_trendings_results1():
+    factory = RequestFactory()
+    request = factory.get('/api/trendings/', {'media_type': 'movie', 'time_window': 'week'})
+    response = view_trendings_results(request)
+
+    assert response.status_code == 200
+    json_data = json.loads(response.content.decode('utf-8'))
+    assert 'results' in json_data
+
+@pytest.mark.django_db
+def test_view_trendings_results2():
+    factory = RequestFactory()
+    request = factory.get('/api/trendings/', {'media_type': 'tv', 'time_window': 'week'})
+    response = view_trendings_results(request)
+
+    assert response.status_code == 200
+    json_data = json.loads(response.content.decode('utf-8'))
+    assert 'results' in json_data
