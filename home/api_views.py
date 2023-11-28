ebe77a7c15a0b87eb16618555608798be270da42
Lakshmi Prasanna Malempati <lm1298@usnh.edu>
2023-11-28 16:50:26 -0500

    create api endpoint for tv comments features

--- home/api_views.py
+++ home/api_views.py
@@ -5,8 +5,14 @@ from rest_framework import status
 from .models import Comment
 from .serializers import CommentSerializer
 
-class CommentAPIView(APIView):
+class CommentAPIViewMovie(APIView):
     def get(self, request, movie_id):
         comments = Comment.objects.filter(movie_id=movie_id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
+    
+class CommentAPIViewTV(APIView):
+    def get(self, request, tv_id):
+        comments = Comment.objects.filter(tv_id=tv_id)
+        serializer = CommentSerializer(comments, many=True)
+        return Response({'comments': serializer.data})
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .api_views import CommentAPIView
+from .api_views import CommentAPIViewMovie, CommentAPIViewTV
 from . import views
 
 urlpatterns = [
@@ -11,5 +11,6 @@ urlpatterns = [
     path("api/trendings/", views.view_trendings_results, name="trendings"),
     path("movie/<int:movie_id>/comments.html", views.comment_page, name="comment_page"),
     path("tv/<int:tv_id>/comments.html", views.comment_page2, name="comment_page2"),
-    path('comments/api/movie/<int:movie_id>/', CommentAPIView.as_view(), name='movie_comments_api'),
+    path('movie/<int:movie_id>/comments/api', CommentAPIViewMovie.as_view(), name='movie_comments_api'),
+    path('tv/<int:tv_id>/comments/api', CommentAPIViewTV.as_view(), name='tv_comments_api'),
 ]
